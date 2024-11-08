3102b87d5bd48a8560ecb4826e3de8d491c4b00a
Antonio Acuña <antony_racuba@hotmail.com>
2024-11-08 16:18:15 -0500

    Add: Handler for creating a customer

---
+++ src/server/rest/customer/handlers.ts
@@ -0,0 +1,48 @@
+import type {
+              Request,
+              Response,
+            } from "express";
+
+import { respondWithStatus } from "../helpers/response";
+
+import {
+         throwIfNoPlainObject,
+         throwIfNoValidNumber,
+         throwIfNoValidString,
+       } from "../../../helpers/user-data";
+
+interface CustomerData
+{
+    document: string;
+    name    : string;
+    email   : string;
+    phone   : number;
+}
+
+export function createCustomer (request: Request, response: Response)
+{
+    const body: any = request.body;
+
+    try
+    {
+        throwIfNoPlainObject(body);
+
+        throwIfNoValidString(body.document);
+        throwIfNoValidString(body.name);
+        throwIfNoValidString(body.email);
+        throwIfNoValidNumber(body.phone);
+    }
+    catch (error: any)
+    {
+        respondWithStatus(request, response, 400, `An error occurred while trying to create a customer: ${(error as Error).message}`)
+    }
+
+    const customerData: CustomerData = {
+        document: body.document,
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+    }
+
+    registerCustomer(customerData);
+}
