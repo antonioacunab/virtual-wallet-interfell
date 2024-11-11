e612cc96633095ec8be5d252d8cbe192292a00db
Antonio Acuña <antony_racuba@hotmail.com>
2024-11-10 23:26:07 -0500

    Update: Path to wsdl files

--- src/server/soap/index.ts
+++ src/server/soap/index.ts
@@ -14,14 +14,24 @@ import { CUSTOMER_SERVICE } from "./customer";
 import { WALLET_SERVICE   } from "./wallet";
 
 /**
- * Path to the file containing the WSDL
+ * Path to the file containing the WSDL for the customer service
  */
-const PATH_TO_WSDL: string = path.join(__dirname, "service.wsdl");
+const PATH_TO_CUSTOMER_WSDL: string = path.join(__dirname, "./customer/service.wsdl");
 
 /**
- * Content of the WSDL file
+ * Path to the file containing the WSDL for the wallet service
  */
-const WSDL_CONTENT: string = fs.readFileSync(PATH_TO_WSDL, "utf-8");
+const PATH_TO_WALLET_WSDL: string = path.join(__dirname, "./wallet/service.wsdl");
+
+/**
+ * Content of the customer WSDL file
+ */
+const CUSTOMER_WSDL_CONTENT: string = fs.readFileSync(PATH_TO_CUSTOMER_WSDL, "utf-8");
+
+/**
+ * Content of the wallet WSDL file
+ */
+const WALLET_WSDL_CONTENT: string = fs.readFileSync(PATH_TO_WALLET_WSDL, "utf-8");
 
 /**
  * Port where the server will be listening
@@ -41,6 +51,6 @@ SERVER.listen(PORT, () => {
     console.log(`SOAP server listening on: ${soapServerUrl}`);
 
     // Start the SOAP servers to listen at the paths /customers and /wallet
-    listen(SERVER, '/customers', CUSTOMER_SERVICE, WSDL_CONTENT);
-    listen(SERVER, '/wallet', WALLET_SERVICE, WSDL_CONTENT);
+    listen(SERVER, '/customers', CUSTOMER_SERVICE, CUSTOMER_WSDL_CONTENT);
+    listen(SERVER, '/wallet', WALLET_SERVICE, WALLET_WSDL_CONTENT);
 });
