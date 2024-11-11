085c2d409d2d9785f06ec1017b558e1cb4daf8dd
Antonio Acuña <antony_racuba@hotmail.com>
2024-11-11 07:01:49 -0500

    Add: Implementation for function "getBalance"

--- src/server/soap/wallet/service.ts
+++ src/server/soap/wallet/service.ts
@@ -1,32 +1,61 @@
-import { CustomServerResponse } from "../../rest/helpers/response";
+import { CustomServerResponse } from "../../../helpers/response";
+import { throwIfNoValidString } from "../../../helpers/user-data";
+import { MY_SQL_SERVICE       } from "../../../services/mysql";
 
-export const SERVICE = {
-    WalletService: {
-        WalletPort: {
-            getBalance (args: any): CustomServerResponse
-            {
-                console.log("Datos recibidos para ver el balance:", args);
+async function getBalance (args: any): Promise<CustomServerResponse>
+{
+    const { document, phone } = args;
 
-                // TODO: Aquí se debe implementar la lógica de negocio
+    try
+    {
+        throwIfNoValidString(document);
+        throwIfNoValidString(phone);
+    }
+    catch (error: any)
+    {
+        return Promise.resolve({
+            success: false,
+            code: 400,
+            message: `The balance could not be obtained: ${error.message}`,
+        });
+    }
+
+    const output: Promise<CustomServerResponse> = MY_SQL_SERVICE.query(
+        `SELECT funds FROM ${process.env.MYSQL_TABLE_NAME} WHERE document = "${document}" AND phone = "${phone}"`
+    )
+        .then((result: any) => {
+            // Throw an error when the document+phone pair does not match any user
+            if (result[0] == null)
+                throw new Error("Either one or both of the document and phone provided are invalid");
+
+            const funds: number = result[0].funds;
 
                 return {
                     success: true,
                     code: 0,
-                    message: "Your balance is: ",
+                message: `${funds}`,
+            };
+        })
+        .catch((error: any) => {
+            return {
+                success: false,
+                code: 500,
+                message: `The balance could not be obtained`,
                 };
-            },
-            addFunds (args: any): CustomServerResponse
-            {
-                console.log("Datos recibidos para agregar fondos:", args);
+        });
 
-                // TODO: Aquí se debe implementar la lógica de negocio
+    return output;
+}
 
                 return {
                     success: true,
                     code: 0,
                     message: "Funds added successfully",
                 };
-            },
+export const SERVICE = {
+    WalletService: {
+        WalletPort: {
+            getBalance,
             pay (args: any): CustomServerResponse
             {
                 console.log("Datos recibidos para pagar:", args);
