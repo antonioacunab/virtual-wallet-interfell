552fc2caaa7e9b4a4fd0414c3505007a83f7d255
Antonio Acuña <antony_racuba@hotmail.com>
2024-11-11 07:02:35 -0500

    Add: Basic implementation of a token generator

---
+++ src/helpers/token.ts
@@ -0,0 +1,9 @@
+export function generateSixNumbersToken (): string
+{
+    let token: string = "";
+
+    for (let i: number = 0; i < 6; i++)
+        token += Math.floor(Math.random() * 10);
+
+    return token;
+}
