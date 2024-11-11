ea30904774e8692a45fac07a73e3909de0674b40
Antonio Acuña <antony_racuba@hotmail.com>
2024-11-11 06:59:54 -0500

    Add: Implementation for function "createCustomer"

--- src/server/soap/customer/service.ts
+++ src/server/soap/customer/service.ts
@@ -1,19 +1,49 @@
-import { CustomServerResponse } from "../../rest/helpers/response";
+import { CustomServerResponse } from "../../../helpers/response";
+import { throwIfNoValidString } from "../../../helpers/user-data";
+import { MY_SQL_SERVICE       } from "../../../services/mysql";
 
 export const SERVICE = {
     CustomerService: {
         CustomerPort: {
-            createCustomer (args: any): CustomServerResponse
+            createCustomer (args: any): Promise<CustomServerResponse>
             {
-                console.log("Datos recibidos para crear cliente:", args);
+                const { document, name, email, phone } = args;
 
-                // TODO: Aquí se debe implementar la lógica de negocio
+                try
+                {
+                    throwIfNoValidString(document);
+                    throwIfNoValidString(name);
+                    throwIfNoValidString(email);
+                    throwIfNoValidString(phone);
+                }
+                catch (error: any)
+                {
+                    return Promise.resolve({
+                        success: false,
+                        code: 400,
+                        message: `A customer could not be created: ${error.message}`,
+                    });
+                }
 
-                return {
-                    success: true,
-                    code: 0,
-                    message: "Customer created successfully",
-                };
+                const output: Promise<CustomServerResponse> = MY_SQL_SERVICE.query(
+                    `INSERT INTO ${process.env.MYSQL_TABLE_NAME} (document, name, email, phone) VALUES ("${document}", "${name}", "${email}", "${phone}")`
+                )
+                    .then(() => {
+                        return {
+                            success: true,
+                            code: 0,
+                            message: "Customer created successfully",
+                        };
+                    })
+                    .catch((error: any) => {
+                        return {
+                            success: false,
+                            code: 500,
+                            message: `A customer could not be created: ${error}`,
+                        };
+                    });
+
+                return output;
             },
         }
     }
